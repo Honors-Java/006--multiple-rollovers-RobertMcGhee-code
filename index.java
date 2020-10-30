d729b3b2cf72a9d80c92de26c52fdc9bfbf370d5
RobertMcGheecod <rlmiii2004@gmail.com>
2020-10-30 14:53:39 +0000

    I recreated the picture

--- index.java
+++ index.java
@@ -7,11 +7,22 @@ void draw() {
 	stroke(0);
 
   //start creating the lines
-
-
+  line(0, 200, 400, 200);
+  line(200, 0, 200, 400);
   //Fill a black color
-  
-
+  if (mouseX < width/2 && mouseY < height/2){
+  fill(0);
+  rect(0, 0, 200, 200);
+  }else if(mouseX > width/2 && mouseY < height/2){
+  fill(0);
+  rect(200, 0, 200, 200);
+  }else if(mouseX < width/2 && mouseY > height/2){
+  fill(0);
+  rect(0, 200, 200, 200);
+  }else{
+  fill(0);
+  rect(200, 200, 200, 200);
+  }
   //Build your conditional statement 
 	
 
